a0058919e5f40fc180c99e5c323533fcc79e295b
sachinbhand <sachinbhand06@gmail.com>
2026-03-06 19:09:41 +0530

    added python logging utility module

---
+++ logging_utils.py
@@ -0,0 +1,20 @@
+import logging
+
+# Configure logging format
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def log_info(message):
+    """
+    Log an informational message
+    """
+    logging.info(message)
+
+
+def log_error(message):
+    """
+    Log an error message
+    """
+    logging.error(message)
---
+++ test_logging.py
@@ -0,0 +1,4 @@
+from logging_utils import log_info, log_error
+
+log_info("Application started successfully")
+log_error("An error occurred in the application")
